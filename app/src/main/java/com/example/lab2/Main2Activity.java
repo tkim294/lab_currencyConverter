3e1e0f11d1d988d4ddb030f400012c8ab428ced6
TAEHYUN KIM <tkim294@wisc.edu>
2020-02-06 18:41:04 -0600

    Initial commit

---
+++ app/src/main/java/com/example/lab2/Main2Activity.java
@@ -0,0 +1,24 @@
+package com.example.lab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    TextView textView2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        textView2 = (TextView) findViewById(R.id.textView2);
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("message");
+        double amount = Double.valueOf(str);
+        textView2.setText("£" + amount * 0.77);
+    }
+}
--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -2,9 +2,24 @@ package com.example.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String str = editText.getText().toString();
+        goToActivity2(str);
+    }
+
+    public void goToActivity2(String str) {
+        Intent intent = new Intent(this, Main2Activity.class);
+        intent.putExtra("message", str);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
